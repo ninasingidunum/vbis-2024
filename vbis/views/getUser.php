0929745a467511305b856607339f50c8292277c1
nina25xx <nina.petrovic.22@singimail.rs>
2024-10-31 20:24:04 +0100

    commit and push

--- vbis/views/getUser.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use app\models\UserModel;
-
-/** @var $params UserModel */
-?>
-
-<h1><?php echo $params->firstName ?> <br? <?php echo $params->lastName?><br><?php echo $params->email?></h1>
--- vbis/views/home.php
+++
@@ -1,5 +0,0 @@
-<?php
-?>
-
-<h1>welcome page!</h1>
-
